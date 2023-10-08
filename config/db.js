bab03de44298d3b91789098559c1c2e796121105
Tuan Nguyen <ngoctuanit07@gmail.com>
2023-10-08 19:31:22 +0700

    update database

--- config/db.js
+++ config/db.js
@@ -5,8 +5,8 @@ const mysql = require('mysql');
 const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'password',
-    database: 'userdb'
+    password: 'qaz123456',
+    database: 'todoapp'
 });
 
 connection.connect();
